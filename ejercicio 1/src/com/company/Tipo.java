b0952c0bb02988c78bcc8f6bf4a3fb6a3fa93706
Oscar Ernesto Gregori Gallardo <46912758+Oscar951@users.noreply.github.com>
2022-08-11 15:35:00 -0600

    Crear proyecto Java con sus tipos de datos

---
+++ ejercicio 1/src/com/company/Tipo.java
@@ -0,0 +1,51 @@
+package com.company;
+
+public class Tipo {
+
+    public static void main(String[] args) {
+        //Enteros
+        byte number1 = 1;
+        System.out.println(number1);
+
+        short number2 = 2;
+        System.out.println(number2);
+
+        int number3 = 3;
+        System.out.println(number3);
+
+        long number4 = 4;
+        System.out.println(number4);
+
+        //Punto flotante
+        float decimal1 = 4.9f;
+        System.out.println(decimal1);
+
+        double decimal2 = 9.99d;
+        System.out.println(decimal2);
+
+        //Caracter
+        char caracter = 'a';
+        System.out.println(caracter);
+
+        //Booleans
+        boolean verdadero = true;
+        System.out.println(verdadero);
+
+        boolean falso =false;
+        System.out.println(falso);
+
+        //Cadena de texto
+        String nombre = "Oscar";
+        System.out.println(nombre);
+
+        String apellido = "Gregori";
+        System.out.println(apellido);
+
+        //Tipos envoltorio
+        Integer numero = null;
+        System.out.println(numero);
+
+        Long numero2 = 2L;
+        System.out.println(numero2);
+    }
+}
